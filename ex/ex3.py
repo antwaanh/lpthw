8a85a72849718cc4515a6a55544d269004d12595
Antwaan Harrison <antwaanvh@gmail.com>
2018-07-03 14:17:49 -0400

    Add example 3

---
+++ ex/ex3.py
@@ -0,0 +1,37 @@
+# Prints out message
+print "I will now count my chickens:"
+
+# Prints out message and calcuates
+print "Hens", 25 + 30 / 6
+# Prints out message and calcuates
+print "Roosters", 100 - 25 * 3 % 4
+
+# Prints out message
+print "Now I will count the eggs:"
+# Performs calcuation
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# Prints out message and calcuates
+print "Is it true that 3 + 2 < 5 - 7?"
+
+# Performs calculation
+print 3 + 2 < 5 - 7
+
+# Prints out message and calcuates
+print "What is 3 + 2?", 3 + 2
+# Prints out message and calcuates
+print "What is 5 - 7?", 5 - 7
+ 
+# Prints out message
+print "Oh, that's why it's False."
+
+# Prints out message and calcuates
+print 'How about some more.'
+
+# Prints out message and calcuates
+print "Is it greater?", 5 > 2
+# Prints out message and calcuates
+print "Is it greater or equal?", 5 >= -2
+# Prints out message and calcuates
+print "Is it less or equal?", 5 <= -2
+
