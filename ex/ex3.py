5973b99a7b817405275b11ed40c8db52f60145c4
Antwaan Harrison <antwaanvh@gmail.com>
2018-07-03 14:20:32 -0400

    Rewrite ex3 to use floats

--- ex/ex3.py
+++ ex/ex3.py
@@ -2,25 +2,25 @@
 print "I will now count my chickens:"
 
 # Prints out message and calcuates
-print "Hens", 25 + 30 / 6
+print "Hens", 25.0 + 30.0 / 6.0
 # Prints out message and calcuates
-print "Roosters", 100 - 25 * 3 % 4
+print "Roosters", 100.0 - 25.0 * 3.0 % 4.0
 
 # Prints out message
 print "Now I will count the eggs:"
 # Performs calcuation
-print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
 
 # Prints out message and calcuates
 print "Is it true that 3 + 2 < 5 - 7?"
 
 # Performs calculation
-print 3 + 2 < 5 - 7
+print 3.0 + 2.0 < 5.0 - 7.0
 
 # Prints out message and calcuates
-print "What is 3 + 2?", 3 + 2
+print "What is 3 + 2?", 3.0 + 2.0
 # Prints out message and calcuates
-print "What is 5 - 7?", 5 - 7
+print "What is 5 - 7?", 5.0 - 7.0
  
 # Prints out message
 print "Oh, that's why it's False."
@@ -29,9 +29,9 @@ print "Oh, that's why it's False."
 print 'How about some more.'
 
 # Prints out message and calcuates
-print "Is it greater?", 5 > 2
+print "Is it greater?", 5.0 > 2.0
 # Prints out message and calcuates
-print "Is it greater or equal?", 5 >= -2
+print "Is it greater or equal?", 5.0 >= -2.0
 # Prints out message and calcuates
-print "Is it less or equal?", 5 <= -2
+print "Is it less or equal?", 5.0 <= -2.0
 
